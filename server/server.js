41300196583319b5421b3f52270af3e4a7feb9f3
New Author Jovi <jovi@gmail.com>
2021-03-06 23:02:04 -0500

    Rest api user

--- server/server.js
+++ server/server.js
@@ -1,44 +1,24 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(require('./controllers/user.controller'));
+
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+console.log(process.env.URLDB);
 
-app.get('/user', (req, res) => {
-    res.json('get user');
-});
-
-app.post('/user', (req, res) => {
-    let body = req.body;
-
-    if (body.name === undefined) {
-        res.status(400).json({
-            ok: false,
-            message: 'name is required',
-        });
-    } else {
-        res.json({
-            user: body
-        });
-    }
-
-});
-
-app.put('/user/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({
-        id,
-        message: 'put user'
-    });
-});
-
-app.delete('/user', (req, res) => {
-    res.json('delete user');
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
 });
 
 app.listen(process.env.PORT, () => console.log('listen in port ' + process.env.PORT));
